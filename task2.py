3b184bf9c1d87d822bd146f4c784c9e124222fb6
Camila Richter <ric23183@uvg.edu.gt>
2026-04-03 12:07:20 -0600

    configuración del min max

--- task2.py
+++ task2.py
@@ -1,3 +1,21 @@
 '''
 TASK 2
+
+- MAX (Defensa)
+- MIN (Hacker)
+- Cada turno captura un nodo vecino
+- Cada nodo tiene un valor
+
 '''
+
+import random
+import time
+import networkx as nx
+
+#configuración con profundidad de bñusqueda 4
+SEED = 42
+NUM_NODOS = 18
+PROBABILIDAD_ARISTA = 0.20
+D_MAX = 4
+
+random.seed(SEED)
