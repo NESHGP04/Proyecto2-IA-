509a515a3354b3af5ca9c6f3c76b81af0e69a475
Camila Richter <ric23183@uvg.edu.gt>
2026-04-03 12:12:50 -0600

    función para generar el grafo

--- task2.py
+++ task2.py
@@ -19,3 +19,16 @@ PROBABILIDAD_ARISTA = 0.20
 D_MAX = 4
 
 random.seed(SEED)
+
+#función para generar grafo
+def generar_grafo(n=NUM_NODOS, p=PROBABILIDAD_ARISTA, seed=SEED):
+    G = nx.erdos_renyi_graph(n, p, seed=seed)
+
+    while not nx.is_connected(G):
+        comp = list(nx.connected_components(G))
+        u = random.choice(list(comp[0]))
+        v = random.choice(list(comp[1]))
+        G.add_edge(u, v)
+
+    mapeo = {i: f"S{i}" for i in range(n)}
+    return nx.relabel_nodes(G, mapeo)
